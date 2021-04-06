7a7ae1b73684cea76610c1071a8602a7d4b85305
Fidela-Keziah <irafidela@gmail.com>
2021-04-06 09:20:39 +0200

    add all files

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:12345@localhost/pitch_app'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fidela:superkeziah/pitch_app'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     #  email configurations
@@ -18,10 +18,10 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:12345@localhost/pitch_app_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fidela:superkeziah@localhost/pitch_app_test'
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:12345@localhost/pitch_app'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fidela:superkeziah@localhost/pitch_app'
 
     DEBUG = True
     
